e646b59e97b2112b571e48cd8f1959ac8cbf3178
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-26 20:03:52 -0300

    add: Status enum

---
+++ Back-End/src/TEOAG.API/Data/Migrations/20230326225912_Twol.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TEOAG.API.Data.Migrations
+{
+    public partial class Twol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Products",
+                newName: "SupplierDescription");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "ProductName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CNPJSupplier",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpirationDate",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManufacturingDate",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductDescription",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupplierId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CNPJSupplier",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ExpirationDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ManufacturingDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductDescription",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "SupplierDescription",
+                table: "Products",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Products",
+                newName: "Description");
+        }
+    }
+}
--- Back-End/src/TEOAG.API/Models/Product.cs
+++ Back-End/src/TEOAG.API/Models/Product.cs
@@ -2,10 +2,16 @@ namespace TEOAG.API.Models
 {
     public class Product
     {
-          public int Id {get; set;}
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Status { get; set; }
+        public int Id {get; set;}
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public Status Status { get; set; }
+        public string ManufacturingDate { get; set; }
+        public string ExpirationDate { get; set; }
+        public int SupplierId { get; set; }
+        public string SupplierDescription { get; set; }
+        public string CNPJSupplier { get; set; }
+
         public Product() { }
 
         public Product(int id)
