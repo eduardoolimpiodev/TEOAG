5e69b374d4516c79038762240bd32be94efd5519
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-26 19:34:54 -0300

    add: props in DataContext

--- Back-End/src/TEOAG.API/Data/DataContext.cs
+++ Back-End/src/TEOAG.API/Data/DataContext.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using TEOAG.API.Models;
 
 namespace TEOAG.API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DbSet<Product> Products { get; set; }
     }
 }
